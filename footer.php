b8d538815dc833b168a59f58df81737028b94199
Amauri Carlos <37253022+AmauriCCJR@users.noreply.github.com>
2024-05-04 19:33:21 -0300

    Adicionando arquivos

---
+++ footer.php
@@ -0,0 +1,23 @@
+<div class="row">
+        <div class="col logofooter">
+            <img  src="images/logoFatecon.png" alt="">
+        </div>
+        <div class="col-5"><ul>
+                    <li><a href="">Início</a></li>
+                    <li><a href="">Em destaque</a></li>
+                    <li><a href="">Artistas</a></li>
+                    <li><a href="">Galeria</a></li>
+                    <li><a href="">História</a></li>
+                    <li><a href="">Eventos</a></li>
+                    <li><a href="">Sobre</a></li>
+                </ul>
+    </div>
+    <div class="col social">
+        <h3 style="color:white;">Nossas Redes</h3>
+        <ul>
+            <li><a href=""><img src="images/face.png" alt=""></a></li>
+            <li><a href=""><img src="images/insta.png" alt=""></a></li>
+            <li><a href=""><img src="images/twitter.png" alt=""></a></li>
+        </ul>
+    </div>
+    </div>
---
+++ header.php
@@ -0,0 +1,32 @@
+<div class="row fatecon">
+        <div class="col logo">
+            <img  src="images/logoFatecon.png" alt="">
+        </div>
+        <div class="col"></div>
+        <div class="col menu">
+            <div class="dropdown">
+                <button class="btn btn-secondary" id="botao-header" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="images/menuHamburguer.png" alt="">
+                </button>
+                <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Quadrinhos em destaque</a></li>
+                <li><a class="dropdown-item" href="#">Artistas</a></li>
+                <li><a class="dropdown-item" href="#">Galeria</a></li>
+                <li><a class="dropdown-item" href="#">História</a></li>
+                <li><a class="dropdown-item" href="#">Eventos</a></li>
+                <li><a class="dropdown-item" href="#">Contato</a></li>
+                <li><a class="dropdown-item" href="#">Quem somos</a></li>
+                </ul>
+            </div>
+        </div> 
+    </div>
+        <nav>
+            <a href="" style="border-left:none;">Inicio</a>
+            <a href="">Em destaque</a>
+            <a href="">Artistas</a>
+            <a href="">Galerias</a>
+            <a href="">História</a>
+            <a href="">Eventos</a>
+            <a href="">Contato</a>
+            <a href="">Sobre nós</a>
+        </nav>
